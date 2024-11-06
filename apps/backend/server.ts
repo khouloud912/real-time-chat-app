83d32bbd9b34aaacae57b1e10bc0e26246939735
khouloud <sedkolod71@gmail.com>
2024-11-06 22:26:46 +0100

    add configuration

--- apps/backend/server.ts
+++ apps/backend/server.ts
@@ -2,19 +2,20 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import connectDB from "./config/database";
+import connectDB from "./src/config/database";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
 connectDB();
+app.use('/auth', authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Chat Server Running");
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running on port 4000");
 });
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx';
 import './styles/index.css';
 import { Auth0ProviderWithNavigate } from './auth/Auth0Provider.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './auth/authContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0ProviderWithNavigate>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </React.StrictMode>
